ccaa2ed07d8452632c7b096c7a785f909fb53ece
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-09-02 13:19:50 +0600

    Loop Prectice

---
+++ forloop.js
@@ -0,0 +1,32 @@
+// node forloop.js
+
+// for(let i = 30; i <=50; i++ ){
+//     // console.log(i);
+// }
+
+// for (let i = 0; i <= 30; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 === 1) {
+//         console.log(i);
+//     }
+// }
+
+// node forloop.js
+
+// for (let i = 20; i >= 15; i++ ){
+
+//     if (i >= 15){
+//         console.log("tahole thik nai");
+//     }   
+// }
+
+// for (let i = 35; i < 99; i += 5){
+
+//     console.log(i);
+// }
---
+++ whileloop.js
@@ -0,0 +1,63 @@
+
+
+// node whileloop.js
+
+// let num = 1
+
+// while (num <= 10) {
+//       console.log(num);
+//       num++
+// }
+
+
+// node whileloop.js
+
+
+// let num = 20
+// // let cost = 0;
+
+// while (num <= 50){
+
+//    // console.log(num);   
+//    num++ 
+// }
+
+
+// node whileloop.js
+
+//   let i = 20;
+
+//   while(i>=10){
+
+//      console.log(i);
+//      i--
+//   }
+
+// node whileloop.js
+
+// let i = 1;
+
+// while (i <= 5) {
+
+//    if (i % 2 === 1) {
+//       console.log("sob bejor songkkha",i);
+//    }
+//    i++
+// }
+
+                                                                                       
+// let i = 0;
+// while (i<=25){
+//    if(i % 2 ==0){
+//       console.log(i);
+//    }
+//    i++
+// }
+
+// node whileloop.js
+
+// let az = 12;
+// while (az < 65) {
+//    console.log(az);
+//    az+=3
+// }
